bf7246f2101b7968ca7d71456b3d7e49bb424a2f
Freya Bull <freya.bull@ed.ac.uk>
2020-07-30 21:39:14 +0100

    Flip axis for subfigs 6 and 8

--- BasicCatheter/visualise/visualise.py
+++ BasicCatheter/visualise/visualise.py
@@ -134,7 +134,7 @@ ax_iw.plot(time[1:], peak_inside[1:]*dx) # Plot location of peak concentration
 ax_iw.fill_between(time, pi_min*dx, pi_max*dx, alpha=0.2) # Add shading indicating peak width
 ax_iw.xaxis.set_major_locator(plt.MultipleLocator(12)) # Format time axis ticks
 ax_iw.set_xlim(left=-1) # Force time to begin at 0
-ax_iw.set_ylim(bottom=-0.05*catheter_length, top=catheter_length*1.05) # Ensure full length of catheter on axis & direction reflects a vertical catheter
+ax_iw.set_ylim(bottom=catheter_length*1.05, top=-0.05*catheter_length) # Ensure full length of catheter on axis & direction reflects a vertical catheter
 ax_iw.set_xlabel('Time (hrs)')
 ax_iw.set_ylabel('Distance from catheter tip (mm)')
 ax_iw.set_title('Position of intraluminal peak concentration')
@@ -195,7 +195,7 @@ for i in range(1,t_len):
 for i in range(6): ax_iwf.fill_between(time, ifrontl[i]*dx, ifrontu[i]*dx, label='$n = 10^{}$ mm$^{{-2}}$'.format(i))
 ax_iwf.xaxis.set_major_locator(plt.MultipleLocator(12)) # Format time axis ticks
 ax_iwf.set_xlim(left=-1) # Force time to begin at 0
-ax_iwf.set_ylim(bottom=-0.05*catheter_length, top=catheter_length*1.05) # Ensure full length of catheter on axis & direction reflects a vertical catheter
+ax_iwf.set_ylim(bottom=catheter_length*1.05, top=-0.05*catheter_length) # Ensure full length of catheter on axis & direction reflects a vertical catheter
 ax_iwf.set_xlabel('Time (hrs)')
 ax_iwf.set_ylabel('Distance from catheter tip (mm)')
 ax_iwf.set_title('Wavefront inside')
